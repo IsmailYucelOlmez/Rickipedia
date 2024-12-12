6c47082585eb384a7e2bca1517f9e71b01c39c76
IsmailYucelOlmez <ismailyucelolmez514@gmail.com>
2024-12-12 17:04:13 +0300

    Character Details Page Completed

--- src/pages/detailspages/CharacterDetailsPage.jsx
+++ src/pages/detailspages/CharacterDetailsPage.jsx
@@ -4,8 +4,13 @@ import { useGetCharacterById } from '../../api/CharacterApi';
 import Image from '../../components/Image';
 import OpenInNewOutlinedIcon from '@mui/icons-material/OpenInNewOutlined';
 import Loading from '../../components/Loading';
-import { useGetEpisodeById, useGetMultipleEpisodes } from '../../api/EpisodeApi';
+import { useGetMultipleEpisodes } from '../../api/EpisodeApi';
 import EpisodeTableRow from '../../components/episode/EpisodeTableRow';
+import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
+import AdbIcon from '@mui/icons-material/Adb';
+import TransgenderIcon from '@mui/icons-material/Transgender';
+import PublicIcon from '@mui/icons-material/Public';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const CharacterDetailsPage = () => {
 
@@ -31,21 +36,32 @@ const CharacterDetailsPage = () => {
           <div className='flex flex-col gap-8'>
             <h5 className='text-xl font-semibold'>{character?.name}</h5>
 
-            <div className='flex flex-col gap-2'>
-              <p>{character?.status}</p>
-              <p>{character?.species}</p>
-              <p>{character?.gender}</p>
+            <div className='flex flex-col gap-4'>
+              <div className='flex items-center gap-2'>
+                <MonitorHeartIcon sx={{ fontSize:{ xs:15, sm:20, md:20} }}/>
+                <p>{character?.status}</p>
+              </div>
+              <div className='flex items-center gap-2'>
+                <AdbIcon sx={{ fontSize:{ xs:15, sm:20, md:20} }}/>
+                <p>{character?.species}</p>
+              </div>
+              <div className='flex items-center gap-2'>          
+                <TransgenderIcon sx={{ fontSize:{ xs:15, sm:20, md:20} }}/>              
+                <p>{character?.gender}</p>
+              </div>     
             </div>
 
-            <div className='flex flex-col gap-3'>
-              <Link to={character?.origin?.url || ''}  className="flex items-center gap-1 px-1" >            
+            <div className='flex flex-col gap-4'>
+              <Link to={character?.origin?.url || ''}  className="flex items-center gap-2 px-1" >    
+                <PublicIcon sx={{ fontSize:{ xs:15, sm:20, md:20} }}/>   
                 Origin: '
                 {character?.origin?.name}'
                 {character?.origin?.url && (
                   <OpenInNewOutlinedIcon sx={{ fontSize:{ xs:15, sm:15, md:15} }}/>
                 )}
               </Link>      
-              <Link to={character?.location?.url || ''} className=" flex items-center gap-1 px-1" >           
+              <Link to={character?.location?.url || ''} className=" flex items-center gap-2 px-1" >       
+                <LocationOnIcon sx={{ fontSize:{ xs:15, sm:20, md:20} }}/>      
                 Location: ' 
                 { character?.location?.name}'
                 {character?.location?.url && (
@@ -58,13 +74,23 @@ const CharacterDetailsPage = () => {
         </div>
 
         <div className='flex flex-col items-center gap-8 w-full'>
-          <h5 className='text-xl font-semibold'>Episodes</h5>
+          <div className='flex items-center justify-center gap-2'>
+            <h5 className='text-xl font-semibold'>Episodes</h5>
+            <p className='text-right'>({character?.episode?.length})</p>
+          </div>
+          
 
           <div className='flex flex-col  w-full'>
-          {episodes?.map((e)=>(                        
+          {episodesLoading ? (
            
-            <EpisodeTableRow key={e.id} episode={e}/>           
-          ))}
+            <Loading/>
+          ):(
+            episodes?.map((e)=>(                        
+           
+              <EpisodeTableRow key={e.id} episode={e}/>           
+            ))
+          )}
+          
           </div>
         </div>
       </>
